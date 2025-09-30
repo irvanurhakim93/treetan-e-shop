08c7c630436f5c4aad564aa5e1f8bfac2301e0a2
irvan.nurhakim1993 <irvan.nurhakim1993@gmail.com>
2025-09-30 18:41:17 +0700

    updates

--- app/Http/Controllers/api/AuthApiController.php
+++ app/Http/Controllers/api/AuthApiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AuthApiController extends Controller
 {
